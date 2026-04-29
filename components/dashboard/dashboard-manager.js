ec94e957442c450cc7d10b7c8ccb6f423213a7e4
Cristian Mulino <cristian.mulino@gmail.com>
2026-04-30 01:10:02 +0200

    Estratto solo nome da physiqueRating quando è oggetto

--- components/dashboard/dashboard-manager.js
+++ components/dashboard/dashboard-manager.js
@@ -408,9 +408,13 @@ bmr = user.bmr + ' kcal';
 // Physique Rating da latest_bia
 let physiqueRating = 'N/D';
 if (user.latest_bia?.physiqueRating) {
-physiqueRating = typeof user.latest_bia.physiqueRating === 'string' 
-? user.latest_bia.physiqueRating 
-: JSON.stringify(user.latest_bia.physiqueRating);
+if (typeof user.latest_bia.physiqueRating === 'string') {
+physiqueRating = user.latest_bia.physiqueRating;
+} else if (typeof user.latest_bia.physiqueRating === 'object' && user.latest_bia.physiqueRating.name) {
+physiqueRating = user.latest_bia.physiqueRating.name;
+} else {
+physiqueRating = JSON.stringify(user.latest_bia.physiqueRating);
+}
 }
 
 console.log(`[DEBUG Card ${d.id}] latest_bia:`, user.latest_bia);
